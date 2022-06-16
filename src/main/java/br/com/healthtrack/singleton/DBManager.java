d7b471567233ec450980ac9d36abf1176167904a
Vitor Franco <vitoroliveirafranco@gmail.com>
2022-06-15 22:55:53 -0300

    finished

--- src/main/java/br/com/healthtrack/singleton/DBManager.java
+++ src/main/java/br/com/healthtrack/singleton/DBManager.java
@@ -24,8 +24,8 @@ public class DBManager {
 
 			connection = DriverManager.getConnection(
 				"jdbc:oracle:thin:@oracle.fiap.com.br:1521:ORCL", 
-				"RM92532",
-				"230601"
+				"USER",
+				"PASSWORD"
 			);
 
 		} catch (Exception e) {
